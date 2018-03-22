6f4489db231b50b37d63fb7a1c2a959519e52b4e
Andrew Joshowitz <ajoshow@sas.upenn.edu>
2018-03-22 18:38:39 +0000

    automatically backed up by learn

---
+++ models/piglatinizer.rb
@@ -0,0 +1,24 @@
+class PigLatinizer
+  def piglatinize(text)
+    words = text.split
+    piglatin = []
+    vowels = ['a', 'e', 'i','o', 'u']
+    words.each do |word|
+      pig = word.split('')
+      i_first_vowel = pig.find_index{|letter| vowels.include?(letter.downcase)}
+      if i_first_vowel == 0
+        pig << "way"
+        piglatin << pig.join
+      else
+        pig.rotate!(i_first_vowel)
+        pig << "ay"
+        piglatin << pig.join
+      end
+    end
+    piglatin.join(' ')
+  end
+
+  def to_pig_latin(sentence)
+    piglatinize(sentence)
+  end
+end
